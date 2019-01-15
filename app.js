e091069a99e4ac75af46ddb0d3d2a3678c8718c6
Andres Valdivieso <aavaldivieso@uc.cl>
2019-01-14 21:49:57 -0300

    valores funcionando

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-app.js
+//var rates {};
+$(document).ready(function(){
+
+	$.ajax({
+		url: 'https://mindicador.cl/api',
+		method: 'GET'
+
+	}).then(function(data){
+		console.log(data);
+		$('#dolar').text(data.dolar.valor);
+		$('#euro').text(data.euro.valor);
+		$('#utm').text(data.utm.valor);
+		$('#uf').text(data.uf.valor);
+	});
+
+	$('#button').on('click', function(e){
+		evento.preventDefault();
+
+	})
---
+++ borrador.js
@@ -0,0 +1,16 @@
+
+		//rates data;
+		//('.indicador.usd').html(formatNumber(rates.dolar.valor));
+		//('.indicador.euro').html(formatNumber(rates.euro.valor));
+		//('.indicador.utm').html(formatNumber(rates.utm.valor));
+		//('.indicador.uf').html(formatNumber(rates.uf.valor));
+	//});
+
+
+	//$('#exchange-form input').keyup(function(e){
+	//	e.preventDefault();
+	//	var amount = $('.clp_input').val();
+	//	$('.exchange-result.usd').html(formatNumber( amount / rates.dolar.valor));
+	//	$('.exchange-result.euro').html(formatNumber( amount / rates.dolar.valor));
+	//	$('.exchange-result.utm').html(formatNumber( amount / rates.dolar.valor));
+	//	$('.exchange-result.uf').html(formatNumber( amount / rates.dolar.valor));
